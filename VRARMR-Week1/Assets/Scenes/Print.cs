dfcd142e7f56cacc43b10d90e98a8f85ef8b8795
Parksolmi <69065439+Parksolmi@users.noreply.github.com>
2021-09-10 13:59:51 +0900

    실습1-print문장출력

---
+++ VRARMR-Week1/Assets/Scenes/Print.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Print : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("VR/AR/MR 프로그래밍");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
